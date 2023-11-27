21f5b42b54bc5ecae6eda80bdb6ddcbc0b337447
Riley Tate <124rtate@frhsd.com>
2023-11-27 09:46:38 -0500

    category

--- js/custom.js
+++ js/custom.js
@@ -1,5 +1,6 @@
 let profilePic = document.getElementById("profile-pic");
 let inputFile = document.getElementById("input-file");
+let selectedPic = document.getElementById("selected-pic")
 
 inputFile.onchange = function(){
     profilePic.src = URL.createObjectURL(inputFile.files[0]);
