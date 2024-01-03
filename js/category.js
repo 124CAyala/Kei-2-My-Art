3bbfffdc8f23ea6023ff78f2f9279ad072d4250f
Chris <124cayala@frhsd.com>
2024-01-02 22:20:12 -0500

    finished item selection functionality

--- js/category.js
+++ js/category.js
@@ -3,41 +3,48 @@ let selected = "imgs/download.jpg";
 var customButton = document.getElementById("custom-btn");
 
 customButton.addEventListener("click", function(){
-    changeSelected(selected);
+    changeSelected();
 })
 
-document.getElementById("champain").addEventListener("click", function(){
-    getSelectedCat(1)
- });
-document.getElementById("henny").addEventListener("click", function(){
-    getSelectedCat(2)
- });
-document.getElementById("titos").addEventListener("click", function(){
-    getSelectedCat(3)
- });
-
-
-function getSelectedCat(x) {
-    switch(x) {
-        case 1:
-            selected = "imgs/champain.jpg";
-            break;
-        case 2:
-            selected = "imgs/henny.jpg";
-            break;
-        case 3:
-            selected = "imgs/titos.jpg";
-            break;
-        default:
-            console.log("err");
-    }
-
-    console.log(x);
+var body = document.getElementsByTagName('body')[0];
+body.onclick = function(e) {
+    if (e.srcElement.tagName == 'IMG');
+    selected = e.srcElement.src;  
     console.log(selected);
-}
+};
+
+// document.getElementById("champain").addEventListener("click", function(){
+//     getSelectedCat(1)
+//  });
+// document.getElementById("henny").addEventListener("click", function(){
+//     getSelectedCat(2)
+//  });
+// document.getElementById("titos").addEventListener("click", function(){
+//     getSelectedCat(3)
+//  });
+
+
+// function getSelectedCat(x) {
+//     switch(x) {
+//         case 1:
+//             selected = "imgs/champain.jpg";
+//             break;
+//         case 2:
+//             selected = "imgs/henny.jpg";
+//             break;
+//         case 3:
+//             selected = "imgs/titos.jpg";
+//             break;
+//         default:
+//             console.log("err");
+//     }
+
+//     console.log(x);
+//     
+// }
 
 
 
-function changeSelected(selected) {
-    console.log(selected);
+function changeSelected() {
+    localStorage.setItem('selected-item', selected);
 }
--- js/custom.js
+++ js/custom.js
@@ -2,6 +2,8 @@ var profilePic = document.getElementById("profile-pic");
 var inputFile = document.getElementById("input-file");
 var selectedPic = document.getElementById("selected-pic");
 
+selectedPic.src = localStorage.getItem('selected-item');
+
 
 inputFile.onchange = function(){
     profilePic.src = URL.createObjectURL(inputFile.files[0]);
