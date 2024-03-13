9ba79a588df22b08cadc75ddfd392f01aa243e80
Chris <124cayala@frhsd.com>
2024-03-13 15:03:33 -0400

    created checkout button

--- js/custom.js
+++ js/custom.js
@@ -8,3 +8,11 @@ selectedPic.src = localStorage.getItem('selected-item');
 inputFile.onchange = function(){
     profilePic.src = URL.createObjectURL(inputFile.files[0]);
 }
+
+function on() {
+    document.getElementById("overlay").style.display = "block";
+  }
+  
+  function off() {
+    document.getElementById("overlay").style.display = "none";
+  }
