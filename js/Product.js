bf0532d35699451785b0ac5ed18f4cfad2d3aec3
Riley Tate <124rtate@frhsd.com>
2024-03-15 10:57:22 -0400

    product css and js

--- js/Product.js
+++ js/Product.js
@@ -1,3 +1,20 @@
 var selectedPic = document.getElementById("selected-pic");
+var plus = document.getElementById("plus");
+var minus = document.getElementById("minus");
+var quantity = document.getElementById("quantity")
 
 selectedPic.src = localStorage.getItem('selected-item');
+
+plus.addEventListener("click", add(quantity))
+
+minus.addEventListener("click", subtract(quantity))
+
+
+function add(quantity){
+    quantity++;
+    document.getElementById("quantity").innerHTML = quantity;
+}
+function subtract(quantity){
+    quantity--;
+    document.getElementById("quantity").innerHTML = quantity;
+}
